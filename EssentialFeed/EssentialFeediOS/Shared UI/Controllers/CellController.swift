986d17251aa16e976128107fda5ab0f727bbeb36
Andrei Ostafciuc <ano@miracle.dk>
2020-11-20 11:25:25 +0200

    Replace protocol composition in a single type with a strict composition to prevent forcing clients to implement methods they don't care about.

---
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/CellController.swift
@@ -0,0 +1,27 @@
+//
+//  CellController.swift
+//  EssentialFeediOS
+//
+//  Created by Andrei Ostafciuc on 20/11/2020.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+public struct CellController {
+  let dataSource: UITableViewDataSource
+  let delegate: UITableViewDelegate?
+  let dataSourcePrefetching: UITableViewDataSourcePrefetching?
+
+  public init(_ dataSource: UITableViewDataSource & UITableViewDelegate & UITableViewDataSourcePrefetching) {
+    self.dataSource = dataSource
+    self.delegate = dataSource
+    self.dataSourcePrefetching = dataSource
+  }
+
+  public init(_ dataSource: UITableViewDataSource ) {
+    self.dataSource = dataSource
+    self.delegate = nil
+    self.dataSourcePrefetching = nil
+  }
+}
